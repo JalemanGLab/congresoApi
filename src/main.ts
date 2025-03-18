ccf8113d48c65040b1f1775e40a344f7b7c5b0b9
Javier Aleman <javier.aleman@gracialab.com.co>
2025-03-18 07:56:34 -0500

    ajuste

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,18 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  // Configuración CORS totalmente abierta
+  app.enableCors({
+    origin: '*',
+    methods: '*',
+    allowedHeaders: '*',
+    exposedHeaders: '*',
+    credentials: false,
+    maxAge: 86400,
+    preflightContinue: true
+  });
+
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
